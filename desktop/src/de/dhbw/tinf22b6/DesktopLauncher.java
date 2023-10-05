719be4de71b68a1f53caf8e940298ef6ceb3739a
Martin Schmidt <martin.schmidt@freemine.de>
2023-10-05 17:19:19 +0200

    initial project files

---
+++ desktop/src/de/dhbw/tinf22b6/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package de.dhbw.tinf22b6;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import de.dhbw.tinf22b6.UnderwatchGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Underwatch");
+		new Lwjgl3Application(new UnderwatchGame(), config);
+	}
+}
