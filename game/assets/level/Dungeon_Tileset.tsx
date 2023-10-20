c1d31b0b9cea3a5afdb5c475660480ac6866e4b4
Martin <martin.schmidt@freemine.de>
2023-10-20 20:28:09 +0200

    move tiled Assets to game root

---
+++ game/assets/level/Dungeon_Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Dungeon_Tileset" tilewidth="16" tileheight="16" tilecount="100" columns="10">
+ <image source="2D Pixel Dungeon Asset Pack/character and tileset/Dungeon_Tileset.png" width="160" height="160"/>
+</tileset>
