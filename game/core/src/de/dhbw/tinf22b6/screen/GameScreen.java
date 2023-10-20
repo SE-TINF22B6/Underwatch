49e6604aae4242750cb2f563baf4090b9c6f4524
Martin Schmidt <martin.schmidt@freemine.de>
2023-10-20 16:53:55 +0200

    Screen implementation

--- game/core/src/de/dhbw/tinf22b6/screen/GameScreen.java
+++ game/core/src/de/dhbw/tinf22b6/screen/GameScreen.java
@@ -1,49 +1,55 @@
-package de.dhbw.tinf22b6;
+package de.dhbw.tinf22b6.screen;
 
-import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import de.dhbw.tinf22b6.world.WorldController;
 import de.dhbw.tinf22b6.world.WorldRenderer;
 
-public class UnderwatchGame extends Game {
-
-    private static final String TAG = UnderwatchGame.class.getName();
+public class GameScreen extends AbstractGameScreen {
+    private static final String TAG = GameScreen.class.getName();
 
     private WorldController worldController;
     private WorldRenderer worldRenderer;
 
     private boolean paused;
 
-    @Override
-    public void create () {
-        // Set Libgdx log level to DEBUG
-        Gdx.app.setLogLevel(Application.LOG_DEBUG);
-        // Initialize controller and renderer
-        worldController = new WorldController();
-        worldRenderer = new WorldRenderer(worldController);
+    public GameScreen (Game game) {
+        super(game);
     }
 
     @Override
-    public void render () {
+    public void render (float deltaTime) {
         // Do not update game world when paused.
         if (!paused) {
             // Update game world by the time that has passed
             // since last rendered frame.
-            worldController.update(Gdx.graphics.getDeltaTime());
+            worldController.update(deltaTime);
         }
         // Sets the clear screen color to: Cornflower Blue
-        Gdx.gl.glClearColor(0x64 / 255.0f, 0x95 / 255.0f, 0xed / 255.0f, 0xff / 255.0f);
+        Gdx.gl.glClearColor(0x64 / 255.0f,
+                0x95 / 255.0f,
+                0xed / 255.0f,
+                0xff / 255.0f);
         // Clears the screen
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         // Render game world to screen
         worldRenderer.render();
     }
-
     @Override
     public void resize (int width, int height) {
         worldRenderer.resize(width, height);
+    }
+
+    @Override
+    public void show () {
+        worldController = new WorldController();
+        worldRenderer = new WorldRenderer(worldController);
+    }
+
+    @Override
+    public void hide () {
+        worldRenderer.dispose();
     }
 
     @Override
@@ -53,13 +59,8 @@ public class UnderwatchGame extends Game {
 
     @Override
     public void resume () {
+        super.resume();
+        // Only called on Android!
         paused = false;
     }
-
-    @Override
-    public void dispose () {
-        worldRenderer.dispose();
-    }
-
 }
-
---
+++ game/core/src/de/dhbw/tinf22b6/util/Assets.java
@@ -0,0 +1,41 @@
+package de.dhbw.tinf22b6.util;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetDescriptor;
+import com.badlogic.gdx.assets.AssetErrorListener;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.utils.Disposable;
+
+public class Assets implements Disposable, AssetErrorListener {
+    public static final String TAG = Assets.class.getName();
+    public static final Assets instance = new Assets();
+    private AssetManager assetManager;
+
+    // singleton: prevent instantiation from other classes
+    private Assets() {
+    }
+
+    public void init(AssetManager assetManager) {
+        this.assetManager = assetManager;
+        // set asset manager error handler
+        assetManager.setErrorListener(this);
+        // load texture atlas
+        assetManager.load(Constants.TEXTURE_ATLAS_OBJECTS, TextureAtlas.class);
+        // start loading assets and wait until finished
+        assetManager.finishLoading();
+        Gdx.app.debug(TAG, "# of assets loaded: " + assetManager.getAssetNames().size);
+        for (String a : assetManager.getAssetNames())
+            Gdx.app.debug(TAG, "asset: " + a);
+    }
+
+    @Override
+    public void dispose() {
+        assetManager.dispose();
+    }
+
+    @Override
+    public void error(AssetDescriptor asset, Throwable throwable) {
+        Gdx.app.error(TAG, "Couldn't load asset '" + asset.fileName + "'", throwable);
+    }
+}
