da1906bcb4eb65311dd1b0a3be13dee811ee42ff
Martin Schmidt <martin.schmidt@freemine.de>
2023-11-02 10:26:46 +0100

    Revert "test"
    This reverts commit 94be6c9bdde824543526f91f07854c05a3b1f977.

--- game/core/src/de/dhbw/tinf22b6/UnderwatchGame.java
+++ game/core/src/de/dhbw/tinf22b6/UnderwatchGame.java
@@ -11,7 +11,7 @@ public class UnderwatchGame extends ApplicationAdapter {
 	
 	@Override
 	public void create () {
-		batch = new SpritBatch();
+		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 	}
 
