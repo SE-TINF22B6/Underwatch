fd85e3e843b0293163e423d68f00970ec71e6200
Jakob Fassunge <87950162+dolerich-hirnfiedler@users.noreply.github.com>
2023-11-14 15:17:13 +0100

    added Testfile and deleted test

--- backend/src/test/java/com/underwatch/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/underwatch/backend/BackendApplicationTests.java
@@ -1,13 +1,12 @@
 package com.underwatch.backend;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
