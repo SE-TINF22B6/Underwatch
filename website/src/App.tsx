f1f47253491de6f04bd20e920223dc3f9977fa1c
JAWolfs <leitwolffe007@gmail.com>
2023-11-20 14:47:16 +0100

    finished mock-up with color-scheme

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -2,30 +2,36 @@ import React from 'react';
 import './App.css';
 import { Link } from 'react-router-dom';
 import {
-  Button
+  Button, ThemeProvider, Typography
 } from '@mui/material';
+import VideoBackground from './VideoBackground';
+import { theme1 } from './theme';
 
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='lp-heading'>Underwatch</h1>
-      <p className='lp-subheading'>The best game from TINF22B6</p>
-      
-      <div style={{display: 'flex', justifyContent: 'center'}}>
-        <Link to="/scoreboard">
-          <Button variant="contained">
-                  Scoreboard
-          </Button>
-        <li></li>
-        </Link>
+    <ThemeProvider theme={theme1}>
+      <div className="App">
+        <VideoBackground/>
+        <Typography variant='h1' style={{color: theme1.palette.primary.main, fontSize: '15vw', marginTop: '20vh', marginBottom: '0'}}>Underwatch</Typography>
+        <Typography variant='subtitle2' style={{color: theme1.palette.primary.main, fontSize: '5vw', marginTop: '0', marginBottom: '30px'}}>The best game from TINF22B6</Typography>
+        {/*<h1 className='lp-heading'>Underwatch</h1>
+        <p className='lp-subheading'>The best game from TINF22B6</p>*/}
+        
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+          <Link to="/scoreboard">
+            <Button variant="contained">
+                    Scoreboard
+            </Button>
+          </Link>
           <Link to="/wiki">
               <Button variant="contained">
                   Wiki
               </Button>
           </Link>
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
--- website/src/Scoreboard.tsx
+++ website/src/Scoreboard.tsx
@@ -11,20 +11,25 @@ import {
   TableHead,
   TableRow,
   Paper,
-  createTheme,
   Button,
   TextField,
   Slider,
+  ThemeProvider,
+  Typography,
+  InputLabel,
+  FormControl,
+  OutlinedInput,
 } from '@mui/material';
-import { theme1 } from './theme';
+import { theme2 } from './theme';
+import VideoBackground from './VideoBackground';
 
-const theme = createTheme();
 
 let data = [
   { name: 'TheDestroyer', date: '2023-11-08', score: '137' },
   { name: 'TheGamerPro', date: '2023-11-06', score: '98' },
   { name: 'TheDestroyer', date: '2023-11-04', score: '241' },
 ];
+
 
 
 
@@ -44,71 +49,99 @@ const Scoreboard = () => {
     };
 
   return (
-    <div className="Scoreboard">
-      <AppBar position="static" className="appBar">
-        <Toolbar variant="dense">
-          <Link to="/" className="link">
-            <img src="../Logo.png" alt="Underwatch-logo" className="logo" />
-          </Link>
-          <p className="sc-heading">Scoreboard</p>
-        </Toolbar>
-      </AppBar>
+    <ThemeProvider theme={theme2}>
+      <div className="Scoreboard">
+        <VideoBackground/>
+        <AppBar position="static" className="appBar">
+          <Toolbar variant="dense">
+            <Link to="/" className="link">
+              <img src="../Logo.png" alt="Underwatch-logo" className="logo" />
+            </Link>
+              <Typography variant='h1' style={{margin: 'auto', fontSize: '35px', fontWeight: 'bold'}}>Scoreboard</Typography>
+            {/*<p className="sc-heading">Scoreboard</p>*/}
+          </Toolbar>
+        </AppBar>
 
-      <div style={{display: 'flex'}}>
-        <TableContainer component={Paper} sx={{ maxWidth: `calc(100% - 40px)`, maxHeight:'800px', margin: '20px', backgroundColor: '#3d253b'}}>
-        <Table sx={{ minWidth: 350, size: 'small'}} aria-label="simple table">
-        <TableHead>
-            <TableRow>
-            <TableCell >Username</TableCell>
-            <TableCell  align="right">
-                Date
-            </TableCell>
-            <TableCell  align="right">
-                Scores
-            </TableCell>
-            </TableRow>
-        </TableHead>
+        <div style={{display: 'flex'}}>
+          <TableContainer component={Paper} sx={{ maxWidth: `calc(100% - 40px)`, maxHeight:'800px', margin: '20px', backgroundColor: theme2.palette.primary.main}}>
+          <Table sx={{ minWidth: 350, size: 'small', color: theme2.palette.primary.contrastText}} aria-label="simple table">
+          <TableHead style={{color: theme2.palette.primary.contrastText}}>
+              <TableRow>
+              <TableCell style={{color: theme2.palette.primary.contrastText, fontWeight: 'bold'}}>Username</TableCell>
+              <TableCell align="right" style={{color: theme2.palette.primary.contrastText, fontWeight: 'bold'}}>
+                  Date
+              </TableCell>
+              <TableCell align="right" style={{color: theme2.palette.primary.contrastText, fontWeight: 'bold'}}>
+                  Scores
+              </TableCell>
+              </TableRow>
+          </TableHead>
 
-        <TableBody>
-            {data.map((row) => (
-            <TableRow
-                key={row.date}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-                <TableCell>{row.name}</TableCell>
-                <TableCell  align="right">
-                {row.date}
-                </TableCell>
-                <TableCell align="right">
-                {row.score}
-                </TableCell>
-            </TableRow>
-            ))}
-        </TableBody>
-        </Table>
-        </TableContainer>
+          <TableBody>
+              {data.map((row) => (
+              <TableRow
+                  key={row.date}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                  <TableCell style={{color: theme2.palette.primary.contrastText}}>{row.name}</TableCell>
+                  <TableCell align="right" style={{color: theme2.palette.primary.contrastText}}>
+                  {row.date}
+                  </TableCell>
+                  <TableCell align="right" style={{color: theme2.palette.primary.contrastText}}>
+                  {row.score}
+                  </TableCell>
+              </TableRow>
+              ))}
+          </TableBody>
+          </Table>
+          </TableContainer>
 
-        {showFilterMask && (
-            <Paper elevation={1} sx={{margin: '20px 20px 0 0', padding:'10px', backgroundColor: '#3d253b'}}>
-                <b>Filter</b><br/>
-                Username<br/>
-                <TextField variant="outlined" multiline={false} rows={1}/>
-                Date<br/>
-                <TextField variant="outlined" multiline={false} rows={1}/>
-                min.score<br/>
+          {showFilterMask && (
+              <Paper elevation={1} sx={{margin: '20px 20px 0 0', padding:'10px', backgroundColor: '#3d253b', maxWidth: '30%'}}>
+                <Typography variant='h6' style={{color: theme2.palette.primary.contrastText, fontWeight: 'bold'}}>Filter</Typography>
+                <FormControl 
+                  sx={{'& .MuiOutlinedInput-root':{
+                        '& fieldset': {borderColor: theme2.palette.primary.contrastText}, 
+                        '&:hover fieldset':{borderColor: theme2.palette.primary.contrastText}, 
+                        '&.Mui-focused fieldset':{borderColor: theme2.palette.primary.contrastText}
+                       }
+                      }}>
+                  <InputLabel htmlFor="component-outlined" style={{color:theme2.palette.primary.contrastText}}>Username</InputLabel>
+                  <OutlinedInput
+                    label="Username"
+                    autoComplete='new-password'
+                    style={{color: theme2.palette.primary.contrastText}}
+                    />
+                </FormControl>
+                <FormControl 
+                  sx={{'& .MuiOutlinedInput-root':{
+                        '& fieldset': {borderColor: theme2.palette.primary.contrastText}, 
+                        '&:hover fieldset':{borderColor: theme2.palette.primary.contrastText}, 
+                        '&.Mui-focused fieldset':{borderColor: theme2.palette.primary.contrastText}
+                       }
+                      }}>
+                  <InputLabel htmlFor="component-outlined" style={{color:theme2.palette.primary.contrastText}}>Date</InputLabel>
+                  <OutlinedInput
+                    label="Date"
+                    autoComplete='new-password'
+                    style={{color: theme2.palette.primary.contrastText}}
+                    />
+                </FormControl>
+                <Typography variant='body1' style={{color:theme2.palette.primary.contrastText}}>min.score</Typography>
                 <div style={{padding:'10px'}}>
-                    <Slider aria-label="min-score" />
+                    <Slider aria-label="min-score" style={{color: theme2.palette.primary.contrastText}}/>
                 </div>
-            </Paper>
-        )}
+              </Paper>
+          )}
 
+        </div>
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+          <Button variant="contained" onClick={clickFilterButton} style={{fontWeight: 'bold'}}>
+                  {filterButtonText}
+          </Button>
+        </div>
       </div>
-      <div style={{display: 'flex', justifyContent: 'center'}}>
-        <Button variant="contained" onClick={clickFilterButton}>
-                {filterButtonText}
-        </Button>
-      </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
