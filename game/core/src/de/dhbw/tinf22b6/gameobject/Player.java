31cf17fb377b778b83a399f93b58aeb01eddd747
Martin Schmidt <martin.schmidt@freemine.de>
2023-11-22 20:18:52 +0100

    feat: play dodging animation

--- game/core/src/de/dhbw/tinf22b6/gameobject/Player.java
+++ game/core/src/de/dhbw/tinf22b6/gameobject/Player.java
@@ -1,10 +1,15 @@
 package de.dhbw.tinf22b6.gameobject;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
+import de.dhbw.tinf22b6.util.Assets;
 import de.dhbw.tinf22b6.util.Constants;
 import de.dhbw.tinf22b6.weapon.Weapon;
 
@@ -12,12 +17,16 @@ import static de.dhbw.tinf22b6.util.Constants.PLAYER_BIT;
 import static de.dhbw.tinf22b6.util.Constants.TILE_SIZE;
 
 public class Player extends GameObject {
+    private static final String TAG = Player.class.getName();
     private Weapon weapon;
+    private boolean dodging;
+    private final Animation<TextureAtlas.AtlasRegion> dodgeAnimation;
 
     public Player(World world, Vector2 position) {
         super("priest1_v1", position, world, Constants.PLAYER_BIT);
         // equip weapon
         //this.weapon = new HandGun();
+        this.dodgeAnimation = new Animation<>(0.3f, Assets.instance.getAnimationAtlasRegion("priest1_dash"));
         this.speed = 50;
         // create Body
         BodyDef bodyDef = new BodyDef();
@@ -39,6 +48,15 @@ public class Player extends GameObject {
         boxShape.dispose();
     }
 
+    @Override
+    public void render(Batch batch) {
+        if (!dodging) {
+            super.render(batch);
+            return;
+        }
+        batch.draw(currentAnimation.getKeyFrame(stateTime), pos.x, pos.y);
+    }
+
     public void applyForce(Vector2 motionVector) {
         body.setLinearVelocity(motionVector.x * speed, motionVector.y * speed);
         pos.x = body.getPosition().x - (float) TILE_SIZE / 2;
@@ -51,5 +69,24 @@ public class Player extends GameObject {
 
     public void setWeapon(Weapon weapon) {
         this.weapon = weapon;
+    }
+
+    public boolean isDodging() {
+        return dodging;
+    }
+
+    public void dodge() {
+        this.dodging = true;
+        this.currentAnimation = dodgeAnimation;
+        new Thread(() -> {
+            try {
+                Thread.sleep((long) currentAnimation.getAnimationDuration() * 1000);
+                currentAnimation = idleAnimation;
+                this.dodging = false;
+                Gdx.app.debug(TAG, "ending dodge");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
     }
 }
