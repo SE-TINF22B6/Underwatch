1963d0a35fbf9deb0b1ed1f7571c0f3ee7a10fbb
iscodeminister <50560278+iscodeminister@users.noreply.github.com>
2023-11-29 20:26:34 +0100

    Update wiki.tsx

--- website/src/wiki.tsx
+++ website/src/wiki.tsx
@@ -2,16 +2,35 @@ import React from 'react';
 import WeaponList from './components/WeaponList.js';
 import EnemyList from './components/EnemyList.js';
 import { getWeapons, getEnemies } from './data/base';
+import { ThemeProvider } from "@emotion/react";
+import { theme2 } from "./theme";
+import VideoBackground from "./VideoBackground";
+import { AppBar, Toolbar, Typography } from "@mui/material";
+import {Link} from "react-router-dom";
+import NavigationMenu from "./NavigationMenu";
 
 function Wiki() {
     const weapons = getWeapons();
     const enemies = getEnemies();
 
     return (
-        <div>
+        <ThemeProvider theme={theme2}>
+        <div className="Wiki">
+            <VideoBackground/>
+            <AppBar position="static" className="appBar">
+                <Toolbar variant="dense">
+                    <Link to="/" className="link">
+                        <img src="../Logo.png" alt="Underwatch-logo" className="logo" />
+                    </Link>
+                    <Typography variant='h1' style={{margin: 'auto', fontSize: '35px', fontWeight: 'bold'}}>Wiki</Typography>
+                    <NavigationMenu/>
+                </Toolbar>
+            </AppBar>
             <WeaponList weapons={weapons} />
             <EnemyList enemies={enemies} />
         </div>
+        </ThemeProvider>
+
     );
 }
 
