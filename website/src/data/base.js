a189f75c2afc926134516a98dae9591d4e8d26c4
iscodeminister <50560278+iscodeminister@users.noreply.github.com>
2023-11-30 10:53:37 +0100

    update wiki

--- website/src/data/base.js
+++ website/src/data/base.js
@@ -1,15 +1,15 @@
 
 //test data
 const weapons = [
-    { id: 1, name: 'Sword', damage: 10 },
-    { id: 2, name: 'Raser', damage: 15 },
-    { id: 3, name: 'Magic Staff', damage: 20 },
+    { id: 1, name: 'Sword', damage: 10, type: 'melee' },
+    { id: 2, name: 'Raser', damage: 15, type: 'long' },
+    { id: 3, name: 'Magic Staff', damage: 20, type: 'long' },
 ];
 
 const enemies = [
-    { id: 1, name: 'Goblin', health: 20 },
-    { id: 2, name: 'Orc', health: 30 },
-    { id: 3, name: 'Dragon', health: 100 },
+    { id: 1, name: 'Goblin', health: 20, damage: 2 },
+    { id: 2, name: 'Orc', health: 30, damage: 5 },
+    { id: 3, name: 'Dragon', health: 100, damage: 20 },
 ];
 export const getWeapons = () => weapons;
 export const getEnemies = () => enemies;
--- website/src/wiki.tsx
+++ website/src/wiki.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import WeaponList from './components/WeaponList.js';
-import EnemyList from './components/EnemyList.js';
 import { getWeapons, getEnemies } from './data/base';
 import { ThemeProvider } from "@emotion/react";
 import { theme2 } from "./theme";
@@ -26,9 +24,56 @@ function Wiki() {
                     <NavigationMenu/>
                 </Toolbar>
             </AppBar>
-            <WeaponList weapons={weapons} />
-            <EnemyList enemies={enemies} />
         </div>
+
+        <div>
+            <section>
+                <h2>Enemys</h2>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Health</th>
+                        <th>Damage</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {enemies.map((enemy, index) => (
+                        <tr key={index}>
+                            <td>{enemy.name}</td>
+                            <td>{enemy.health}</td>
+                            <td>{enemy.damage}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </section>
+
+            <section>
+                <h2>Weapons</h2>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Damage</th>
+                        <th>Type</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {weapons.map((weapon, index) => (
+                        <tr key={index}>
+                            <td>{weapon.name}</td>
+                            <td>{weapon.damage}</td>
+                            <td>{weapon.type}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </section>
+        </div>
+
+
+
         </ThemeProvider>
 
     );
