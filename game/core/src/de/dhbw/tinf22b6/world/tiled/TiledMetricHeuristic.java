284eb23aa022d9b4ed19682715e9f121092f7bd4
Jakob Fassunge <87950162+dolerich-hirnfiedler@users.noreply.github.com>
2024-04-23 16:14:54 +0200

    yoink: RaycastCollisionDetector

--- game/core/src/de/dhbw/tinf22b6/world/tiled/TiledMetricHeuristic.java
+++ game/core/src/de/dhbw/tinf22b6/world/tiled/TiledMetricHeuristic.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.ai.pfa.Heuristic;
 /**
  * MetricHeuristic
  */
-public class MetricHeuristic<N extends TiledNode<N>> implements Heuristic<N> {
+public class TiledMetricHeuristic<N extends TiledNode<N>> implements Heuristic<N> {
 
     @Override
     public float estimate(N node, N endNode) {
--- game/core/src/de/dhbw/tinf22b6/world/tiled/TiledRaycastCollisionDetector.java
+++ game/core/src/de/dhbw/tinf22b6/world/tiled/TiledRaycastCollisionDetector.java
@@ -1,5 +1,4 @@
 /*******************************************************************************
- package de.dhbw.tinf22b6.world.tiled;
  * Copyright 2014 See AUTHORS file.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ******************************************************************************/
+
 package de.dhbw.tinf22b6.world.tiled;
 
 import com.badlogic.gdx.ai.utils.Collision;
@@ -27,64 +27,63 @@ import com.badlogic.gdx.math.Vector2;
  *
  * @author davebaol */
 public class TiledRaycastCollisionDetector<N extends TiledNode<N>> implements RaycastCollisionDetector<Vector2> {
-    TiledGraph<N> worldMap;
+	TiledGraph<N> worldMap;
 
-    public TiledRaycastCollisionDetector (TiledGraph<N> worldMap) {
-        this.worldMap = worldMap;
-    }
+	public TiledRaycastCollisionDetector (TiledGraph<N> worldMap) {
+		this.worldMap = worldMap;
+	}
 
-    // See http://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
-    @Override
-    public boolean collides (Ray<Vector2> ray) {
-        int x0 = (int)ray.start.x;
-        int y0 = (int)ray.start.y;
-        int x1 = (int)ray.end.x;
-        int y1 = (int)ray.end.y;
+	// See http://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+	@Override
+	public boolean collides (Ray<Vector2> ray) {
+		int x0 = (int)ray.start.x;
+		int y0 = (int)ray.start.y;
+		int x1 = (int)ray.end.x;
+		int y1 = (int)ray.end.y;
 
-        int tmp;
-        boolean steep = Math.abs(y1 - y0) > Math.abs(x1 - x0);
-        if (steep) {
-            // Swap x0 and y0
-            tmp = x0;
-            x0 = y0;
-            y0 = tmp;
-            // Swap x1 and y1
-            tmp = x1;
-            x1 = y1;
-            y1 = tmp;
-        }
-        if (x0 > x1) {
-            // Swap x0 and x1
-            tmp = x0;
-            x0 = x1;
-            x1 = tmp;
-            // Swap y0 and y1
-            tmp = y0;
-            y0 = y1;
-            y1 = tmp;
-        }
+		int tmp;
+		boolean steep = Math.abs(y1 - y0) > Math.abs(x1 - x0);
+		if (steep) {
+			// Swap x0 and y0
+			tmp = x0;
+			x0 = y0;
+			y0 = tmp;
+			// Swap x1 and y1
+			tmp = x1;
+			x1 = y1;
+			y1 = tmp;
+		}
+		if (x0 > x1) {
+			// Swap x0 and x1
+			tmp = x0;
+			x0 = x1;
+			x1 = tmp;
+			// Swap y0 and y1
+			tmp = y0;
+			y0 = y1;
+			y1 = tmp;
+		}
 
-        int deltax = x1 - x0;
-        int deltay = Math.abs(y1 - y0);
-        int error = 0;
-        int y = y0;
-        int ystep = (y0 < y1 ? 1 : -1);
-        for (int x = x0; x <= x1; x++) {
-            N tile = steep ? worldMap.getNode(y, x) : worldMap.getNode(x, y);
-            if (tile.type != TiledNode.TILE_FLOOR) return true; // We've hit a wall
-            error += deltay;
-            if (error + error >= deltax) {
-                y += ystep;
-                error -= deltax;
-            }
-        }
+		int deltax = x1 - x0;
+		int deltay = Math.abs(y1 - y0);
+		int error = 0;
+		int y = y0;
+		int ystep = (y0 < y1 ? 1 : -1);
+		for (int x = x0; x <= x1; x++) {
+			N tile = steep ? worldMap.getNode(y, x) : worldMap.getNode(x, y);
+			if (tile.type != TiledNode.TILE_FLOOR) return true; // We've hit a wall
+			error += deltay;
+			if (error + error >= deltax) {
+				y += ystep;
+				error -= deltax;
+			}
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    @Override
-    public boolean findCollision (Collision<Vector2> outputCollision, Ray<Vector2> inputRay) {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public boolean findCollision (Collision<Vector2> outputCollision, Ray<Vector2> inputRay) {
+		throw new UnsupportedOperationException();
+	}
 }
-
