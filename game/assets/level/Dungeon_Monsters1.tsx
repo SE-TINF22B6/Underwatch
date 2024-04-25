5ac78cecd429aa3dad65d60bad6b1f759a42249c
Benjamin <bennymp19@gmail.com>
2024-04-25 10:22:02 +0200

    monster.png

--- game/assets/level/Dungeon_Monsters1.tsx
+++ game/assets/level/Dungeon_Monsters1.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Dungeon_Monsters1" tilewidth="24" tileheight="32" tilecount="96" columns="12">
- <image source="Sprites/DampDungeonsRPGMakerMZ/Characters/Dungeon_Monsters1.png" width="288" height="256"/>
+<tileset version="1.10" tiledversion="1.10.2" name="Dungeon_Monsters1" tilewidth="24" tileheight="32" tilecount="30" columns="6">
+ <image source="../tileSets/tilesetsDungeon/Dungeon_Tileset.png" width="160" height="160"/>
 </tileset>
