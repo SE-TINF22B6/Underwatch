a7e864f12112c9db82d415fa232942670e9aef5d
Benjamin <bennymp19@gmail.com>
2024-04-25 10:25:58 +0200

    path update

--- game/assets/level/fences.tsx
+++ game/assets/level/fences.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="fences" tilewidth="16" tileheight="16" tilecount="16" columns="4">
- <image source="Sprites/mystic_woods_free_2.1/sprites/tilesets/fences.png" width="64" height="64"/>
+ <image source="../tileSets/tilesetsAussenwelt/fences.png" width="64" height="64"/>
 </tileset>
