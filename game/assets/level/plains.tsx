130fa44c64d5b7c0f6f06752c8757bbc8da3df80
Benjamin <bennymp19@gmail.com>
2024-04-25 10:37:14 +0200

    hitboxen

--- game/assets/level/plains.tsx
+++ game/assets/level/plains.tsx
@@ -1,6 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="plains" tilewidth="16" tileheight="16" tilecount="72" columns="6">
  <image source="../tileSets/tilesetsAussenwelt/plains.png" width="96" height="192"/>
+ <tile id="24">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3" y="7" width="11" height="9"/>
+  </objectgroup>
+ </tile>
  <tile id="25">
   <objectgroup draworder="index" id="2">
    <object id="1" x="3" y="3" width="13" height="13"/>
@@ -19,6 +24,7 @@
  <tile id="30">
   <objectgroup draworder="index" id="2">
    <object id="1" x="1" y="0" width="15" height="16"/>
+   <object id="2" x="2" y="0" width="13" height="16"/>
   </objectgroup>
  </tile>
  <tile id="31">
@@ -29,6 +35,11 @@
  <tile id="33">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="15" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="36">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3" y="0" width="12" height="14"/>
   </objectgroup>
  </tile>
  <tile id="37">
@@ -46,9 +57,21 @@
    <object id="1" x="0" y="0" width="15" height="15"/>
   </objectgroup>
  </tile>
+ <tile id="43">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3" y="0" width="13" height="17"/>
+  </objectgroup>
+ </tile>
  <tile id="44">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="0" y="0" width="16" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="45">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="14" height="15"/>
+   <object id="2" x="0" y="0" width="14" height="16"/>
   </objectgroup>
  </tile>
 </tileset>
