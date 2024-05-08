eb810d0102cb730cb2f5b27caf580812cdc8eb92
Martin Schmidt <martin.schmidt@freemine.de>
2024-05-08 10:31:30 +0200

    feat: initial map test

--- game/core/src/test/de/dhbw/tinf22b6/world/WorldParserTest.java
+++ game/core/src/test/de/dhbw/tinf22b6/world/WorldParserTest.java
@@ -1,65 +1,20 @@
 package de.dhbw.tinf22b6.world;
 
-import com.badlogic.gdx.Files;
-import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.backends.headless.HeadlessApplication;
-import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
-import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.maps.MapLayer;
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.World;
-import de.dhbw.TestApplication;
-import de.dhbw.tinf22b6.UnderwatchGame;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.mock;
 
 class WorldParserTest {
-
-    private HeadlessApplication headlessApplication;
-    private World world;
     private TiledMap map;
-
-    /**
-     * Helper method for {@link Files#internal(String) Gdx.files.internal(String)}.
-     * Returns a {@link FileHandle}.
-     */
-    public FileHandle internal(String path) {
-        initApplication();
-        FileHandle handle = Gdx.files.internal(path);
-        if (handle == null) {
-            throw new NullPointerException(String.format("internal(%s) returned null: %s", path, handle));
-        }
-        return handle;
-    }
-
-    /**
-     * Initialise a test headless application.
-     */
-    public void initApplication() {
-        if (headlessApplication == null) {
-            HeadlessApplicationConfiguration config = new HeadlessApplicationConfiguration();
-            headlessApplication = new HeadlessApplication(new Game() {
-                @Override
-                public void create() {
-                }
-            }, config);
-            assertNotNull(Gdx.files);
-        }
-    }
 
     @BeforeEach
     void setUp() {
-        initApplication();
-        this.world = new World(new Vector2(0, 0), true);
         this.map = new TiledMap();
         MapLayer background = new MapLayer();
         background.setName("floor");
@@ -71,14 +26,16 @@ class WorldParserTest {
         map.getLayers().add(walls);
     }
 
-    @AfterEach
-    void tearDown() {
+    @Test
+    @DisplayName("Setup of test map is correct")
+    void setupCorrect() {
+        assertEquals("floor", map.getLayers().get(0).getName());
+        assertEquals("walls", map.getLayers().get(1).getName());
+        assertEquals(1, map.getLayers().get(1).getObjects().getCount());
     }
 
     @Test
     void parseStaticObjects() {
-        assertEquals(map.getLayers().get(0).getName(), "floor");
-        assertEquals(map.getLayers().get(1).getName(), "walls");
     }
 
     @Test
