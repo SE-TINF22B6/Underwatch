5bf662b821fd8623933301375c1727250a41caa5
Martin Schmidt <martin.schmidt@freemine.de>
2024-05-08 10:00:44 +0200

    feat: testing ?

--- game/core/src/test/de/dhbw/tinf22b6/world/WorldParserTest.java
+++ game/core/src/test/de/dhbw/tinf22b6/world/WorldParserTest.java
@@ -1,24 +1,58 @@
 package de.dhbw.tinf22b6.world;
 
+import com.badlogic.gdx.Files;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.maps.MapLayer;
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
+import de.dhbw.TestApplication;
+import de.dhbw.tinf22b6.UnderwatchGame;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class WorldParserTest {
 
+    private HeadlessApplication headlessApplication;
     private World world;
     private TiledMap map;
 
+    /**
+     * Helper method for {@link Files#internal(String) Gdx.files.internal(String)}.
+     * Returns a {@link FileHandle}.
+     */
+    public FileHandle internal(String path) {
+        initApplication();
+        FileHandle handle = Gdx.files.internal(path);
+        if (handle == null) {
+            throw new NullPointerException(String.format("internal(%s) returned null: %s", path, handle));
+        }
+        return handle;
+    }
+
+    /**
+     * Initialise a test headless application.
+     */
+    public void initApplication() {
+        if (headlessApplication == null) {
+            HeadlessApplicationConfiguration config = new HeadlessApplicationConfiguration();
+            headlessApplication = new HeadlessApplication(new UnderwatchGame(), config);
+            assertNotNull(Gdx.files);
+        }
+    }
+
     @BeforeEach
     void setUp() {
+        initApplication();
         this.world = new World(new Vector2(0, 0), true);
         this.map = new TiledMap();
         MapLayer background = new MapLayer();
