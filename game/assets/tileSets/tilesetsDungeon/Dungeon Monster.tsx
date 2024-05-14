6f3e85ac6e9e5c0625ca6ff8ad319fd7d5c9cddd
Martin Schmidt <martin.schmidt@freemine.de>
2024-05-14 13:23:15 +0200

    feat: add dungeon Monster TileSet

---
+++ game/assets/tileSets/tilesetsDungeon/Dungeon Monster.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Dungeon Monster" tilewidth="24" tileheight="32" tilecount="96" columns="12">
+ <image source="Dungeon_Monsters.png" width="288" height="256"/>
+</tileset>
