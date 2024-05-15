bad2d9782744f7a7dd1ff54b150d1d44c1d6a1a1
= <leitwolffe007@gmail.com>
2024-05-15 11:44:39 +0200

    feat: added handling for picturePath from database to WikiCard

--- website/src/WikiCardBox.tsx
+++ website/src/WikiCardBox.tsx
@@ -5,12 +5,13 @@ function WikiCardBox(props:{getData:Function}) {
     const data:object[] = props.getData();
     let cardAttributes: string[] = [];
     let cardValues: string[][] = [];
-    cardAttributes.push(Object.keys(data[0])[3], Object.keys(data[0])[2]);
+    let picturePaths: string[] = [];
+    cardAttributes.push(Object.keys(data[0])[2], Object.keys(data[0])[3]);
     data.forEach((object) => {
         const values = Object.values(object);
-        cardValues.push([values[3], values[2]]);
+        cardValues.push([values[2], values[3]]);
+        picturePaths.push(values[values.length-1]);
     });
-    console.log(cardAttributes);
 
     return (
         <div className="OuterCardBox" style={{
@@ -32,7 +33,7 @@ function WikiCardBox(props:{getData:Function}) {
                 justifyContent: "center"
             }}>
                 {data.map((object, index) => 
-                    <WikiCard title={Object.values(object)[1]} attributes={cardAttributes} values={cardValues[index]}/>
+                    <WikiCard title={Object.values(object)[1]} attributes={cardAttributes} values={cardValues[index]} pictureLink={picturePaths[index]}/>
                 )}
             </div>
         </div>
