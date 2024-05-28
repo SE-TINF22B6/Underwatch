b1189eda56af49eac2e2b4cc8ed56b361494282f
Martin Schmidt <schmidt.martin.a22@student.dhbw-karlsruhe.de>
2024-05-28 11:24:35 +0000

    fix: remove build artifacts from repo

--- game/desktop/bin/main/level/babo.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="babo" tilewidth="24" tileheight="32" tilecount="1" columns="1">
- <image source="../enemy/monster/babo/Babo_walk_front_2.png" width="24" height="32"/>
- <tile id="0">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="1" y="5" width="21" height="26"/>
-  </objectgroup>
- </tile>
-</tileset>
