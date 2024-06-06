47f05afddb0286c099bfaf09452ea8cbfc8acdbf
Martin Schmidt <martin.schmidt@freemine.de>
2024-06-06 10:58:16 +0200

    feat: fullscreen

--- game/desktop/src/de/dhbw/tinf22b6/DesktopLauncher.java
+++ game/desktop/src/de/dhbw/tinf22b6/DesktopLauncher.java
@@ -13,8 +13,8 @@ public class DesktopLauncher {
     public static void main(String[] arg) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
         config.setForegroundFPS(60);
-        config.setWindowedMode(800, 600);
-        //config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
+        //config.setWindowedMode(800, 600);
+        config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
         config.setTitle("Underwatch");
         config.setWindowIcon("badlogic.jpg");
         new Lwjgl3Application(new UnderwatchGame(), config);
