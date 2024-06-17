f5d93a42738d6014b5d366fdf8e8aa7b542058ff
Martin Schmidt <martin.schmidt@freemine.de>
2024-06-17 11:26:52 +0200

    feat: make bullet spawn in barrel, add sniper Bullet

--- game/core/src/main/de/dhbw/tinf22b6/weapon/Sniper.java
+++ game/core/src/main/de/dhbw/tinf22b6/weapon/Sniper.java
@@ -6,6 +6,7 @@ import static com.badlogic.gdx.math.MathUtils.sinDeg;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.math.Vector2;
 import de.dhbw.tinf22b6.gameobject.bullet.PlayerBullet;
+import de.dhbw.tinf22b6.gameobject.bullet.SniperBullet;
 import de.dhbw.tinf22b6.util.Assets;
 import de.dhbw.tinf22b6.util.Constants;
 import de.dhbw.tinf22b6.util.EntitySystem;
@@ -24,8 +25,8 @@ public class Sniper extends Weapon {
                     Thread.sleep((long) (shootingAnimation.getAnimationDuration() * 1000));
                     float angle = EntitySystem.instance.getPlayer().getAngle();
                     Vector2 pos = EntitySystem.instance.getPlayer().getPos();
-                    int r = 100;
-                    EntitySystem.instance.add(new PlayerBullet(
+                    int r = 30;
+                    EntitySystem.instance.add(new SniperBullet(
                             new Vector2(pos.x + r * cosDeg(angle), pos.y + 5 + r * sinDeg(angle)),
                             angle,
                             this.getDamage(),
